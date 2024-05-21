94502a0c3d07009eb5f28dfd7c3d9bbffc796383
Peter Huynh <peterhyh@yahoo.com>
2024-05-20 19:17:37 -0500

    User added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from turtle import Screen
+from turtle import *
 
 screen = Screen()
 screen.setup(width=700, height=700)
+screen.bgcolor("lightgreen")
+
+player = Turtle()
+player.color("blue")
+player.shape("triangle")
 
 screen.exitonclick()
