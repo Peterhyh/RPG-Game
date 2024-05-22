86c8ab5bba59c0bc44f3812867eb73d523efaa1c
Peter Huynh <peterhyh@yahoo.com>
2024-05-21 23:24:43 -0500

    surface color is now red

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from sys import exit
 
+pygame.init()
 display = pygame.display
 clock = pygame.time.Clock()
 
-pygame.init()
 screen = display.set_mode((800, 400))
 display.set_caption("Forbidden Dynasty")
 
+surface = pygame.Surface((100, 200))
+surface.fill("Red")
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(surface, (0, 0))
     display.update()
     clock.tick(60)
