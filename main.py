6be019ada55903dca76ff3a4915c29c0336938c9
Peter Huynh <peterhyh@yahoo.com>
2024-05-21 23:11:31 -0500

    clock and title added

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pygame
+from sys import exit
 
 display = pygame.display
+clock = pygame.time.Clock()
 
 pygame.init()
 screen = display.set_mode((800, 400))
+display.set_caption("Forbidden Dynasty")
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     display.update()
+    clock.tick(60)
