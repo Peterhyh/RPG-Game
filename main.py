c11b3226960468cf5a37a284edc8a18b3aa21d24
Peter Huynh <peterhyh@yahoo.com>
2024-05-22 22:15:29 -0500

    NPC added

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ clock = pygame.time.Clock()
 screen = display.set_mode((1404, 716))
 display.set_caption("Forbidden Dynasty")
 
-background_surface = pygame.image.load("images/background.png")
-character_surface = pygame.image.load("images/blob.png")
+background_surface = pygame.image.load("images/background.png").convert()
+character_surface = pygame.image.load("images/blob.png").convert_alpha()
+attack_blob_surface = pygame.image.load(
+    "images/attackBlob.png").convert_alpha()
+attack_blob_x_value = 1000
+attack_blob_y_value = 475
+
+attack_blob_right_surface = pygame.image.load(
+    "images/attackBlobRight.png").convert_alpha()
+attack_blob_right_x_value = 800
+attack_blob_right_y_value = 475
+
+moveLeft = True
 
 # surface = pygame.Surface((100, 200))
 # surface.fill("Red")
@@ -22,6 +33,23 @@ while True:
 
     # screen.blit(surface, (200, 100))
     screen.blit(background_surface, (0, 0))
+
+    if moveLeft == True:
+        screen.blit(attack_blob_surface,
+                    (attack_blob_x_value, attack_blob_y_value))
+        attack_blob_x_value -= 2
+    else:
+        screen.blit(attack_blob_right_surface,
+                    (attack_blob_right_x_value, attack_blob_right_y_value))
+        attack_blob_right_x_value += 2
+
+    if attack_blob_x_value == 800:
+        attack_blob_x_value = 1000
+        moveLeft = False
+    elif attack_blob_right_x_value == 1000:
+        attack_blob_right_x_value = 800
+        moveLeft = True
+
     screen.blit(character_surface, (100, 475))
     display.update()
     clock.tick(60)
